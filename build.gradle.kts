ff60ff7da82341f49c584b61d166c0ffb7b4dbe0
Ruh0n <wns8256@naver.com>
2024-01-19 17:10:35 +0900

    :heavy_plus_sign: Add JPA dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,9 @@ dependencies {
     runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // JPA 구현체인 hibernate
+    implementation ("org.hibernate:hibernate-core:6.1.7.Final")
 }
 
 tasks.withType<Test> {
