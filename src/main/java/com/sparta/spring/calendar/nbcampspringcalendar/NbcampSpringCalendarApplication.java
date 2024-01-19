78cca83b56e469966e7c32087f8608f28e152668
ruh0n <wns8256@naver.com>
2024-01-19 16:23:59 +0900

    :tada: Initial Commit

---
+++ src/main/java/com/sparta/spring/calendar/nbcampspringcalendar/NbcampSpringCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spring.calendar.nbcampspringcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcampSpringCalendarApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NbcampSpringCalendarApplication.class, args);
+  }
+
+}
