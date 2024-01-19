fe1b5d81a089ad9dde0bafff07fa94f2d0aa3297
ruh0n <wns8256@naver.com>
2024-01-19 16:23:59 +0900

    :tada: Initial Commit

---
+++ src/test/java/com/sparta/spring/calendar/nbcampspringcalendar/NbcampSpringCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring.calendar.nbcampspringcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcampSpringCalendarApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
