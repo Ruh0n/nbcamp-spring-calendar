067da03a32b8483ceaa097fb754100cebde0cdce
Ruh0n <wns8256@naver.com>
2024-01-21 00:14:20 +0900

    :sparkles: Add Schedule dto and entity

--- src/main/java/com/sparta/spring/calendar/nbcampspringcalendar/dto/ScheduleResponseDto.java
+++ src/main/java/com/sparta/spring/calendar/nbcampspringcalendar/dto/ScheduleResponseDto.java
@@ -1,5 +1,25 @@
 package com.sparta.spring.calendar.nbcampspringcalendar.dto;
 
+import com.sparta.spring.calendar.nbcampspringcalendar.entity.Schedule;
+import com.sparta.spring.calendar.nbcampspringcalendar.entity.Timestamped;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter(AccessLevel.PRIVATE)
 public class ScheduleResponseDto {
+
+  private Long id;
+  private String title;
+  private String description;
+  private String user;
+
+  public ScheduleResponseDto(Schedule schedule) {
+    this.id = schedule.getId();
+    this.title = schedule.getTitle();
+    this.description = schedule.getDescription();
+    this.user = schedule.getUser();
+  }
 
 }
--- src/main/java/com/sparta/spring/calendar/nbcampspringcalendar/entity/Schedule.java
+++ src/main/java/com/sparta/spring/calendar/nbcampspringcalendar/entity/Schedule.java
@@ -1,5 +1,7 @@
 package com.sparta.spring.calendar.nbcampspringcalendar.entity;
 
+import com.sparta.spring.calendar.nbcampspringcalendar.dto.ScheduleRequestDto;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,10 +17,29 @@ import lombok.Setter;
 @Setter
 @Table(name = "nbcampspringcalendar") // 매핑할 테이블의 이름을 지정
 @NoArgsConstructor
-public class Schedule {
+public class Schedule extends Timestamped {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
+  private Long id;
 
+  @Column
+  private String title;
+  private String description;
+  private String user;
+  private String password;
+
+  public Schedule(ScheduleRequestDto scheduleRequestDto) {
+    this.title = scheduleRequestDto.getTitle();
+    this.description = scheduleRequestDto.getDescription();
+    this.user = scheduleRequestDto.getUser();
+    this.password = scheduleRequestDto.getPassword();
+  }
+
+  public void update(ScheduleRequestDto scheduleRequestDto) {
+    this.title = scheduleRequestDto.getTitle();
+    this.description = scheduleRequestDto.getDescription();
+    this.user = scheduleRequestDto.getUser();
+    this.password = scheduleRequestDto.getPassword();
+  }
 }
