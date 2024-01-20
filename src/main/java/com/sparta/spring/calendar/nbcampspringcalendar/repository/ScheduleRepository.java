8836f3d8f48e0689fc9dc8dc34b76d6c6c6ee274
Ruh0n <wns8256@naver.com>
2024-01-21 00:15:39 +0900

    :sparkles: Add Schedule `repository`

--- src/main/java/com/sparta/spring/calendar/nbcampspringcalendar/repository/ScheduleRepository.java
+++ src/main/java/com/sparta/spring/calendar/nbcampspringcalendar/repository/ScheduleRepository.java
@@ -1,8 +1,10 @@
 package com.sparta.spring.calendar.nbcampspringcalendar.repository;
 
+import com.sparta.spring.calendar.nbcampspringcalendar.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ScheduleRepository {
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
 }
