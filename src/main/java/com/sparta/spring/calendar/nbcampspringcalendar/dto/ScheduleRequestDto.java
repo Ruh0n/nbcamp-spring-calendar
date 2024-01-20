7ab7aa2351f320bd2cc9f338dbf2238a7ae90228
Ruh0n <wns8256@naver.com>
2024-01-21 00:14:20 +0900

    :sparkles: Add Schedule dto and entity

--- src/main/java/com/sparta/spring/calendar/nbcampspringcalendar/dto/ScheduleRequestDto.java
+++ src/main/java/com/sparta/spring/calendar/nbcampspringcalendar/dto/ScheduleRequestDto.java
@@ -1,5 +1,13 @@
 package com.sparta.spring.calendar.nbcampspringcalendar.dto;
 
+import lombok.Getter;
+
+@Getter
 public class ScheduleRequestDto {
+
+  private String title;
+  private String description;
+  private String user;
+  private String password;
 
 }
