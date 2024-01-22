df85e6b60cd540a96899d481fdc2674cb12bd204
Ruh0n <wns8256@naver.com>
2024-01-22 21:59:15 +0900

    :see_no_evil: getAllSchedules order by DESC

--- src/main/java/com/sparta/spring/calendar/nbcampspringcalendar/repository/ScheduleRepository.java
+++ src/main/java/com/sparta/spring/calendar/nbcampspringcalendar/repository/ScheduleRepository.java
@@ -4,7 +4,11 @@ import com.sparta.spring.calendar.nbcampspringcalendar.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    List<Schedule> findAllByOrderByCreatedAtDesc();
 
 }
